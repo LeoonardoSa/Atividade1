29230474f163c21c5276817464b3de2d622555e6
Rodrigo Torres Savone <rodrigo.savone@sinqia.com.br>
2021-09-09 20:58:34 -0300

    alterao versao final

--- Atividade.py
+++ Atividade.py
@@ -64,6 +64,45 @@ while (iteracoes < 7 and sala < 9) and sala != 6:
         sala +=2    
         print("Você está na sala: ", sala)
 
+while iteracoes < 7 and sala < 8:
+    
+    print("Escolha seu caminho: ")
+    print("[2] - Caminho preto")
+    caminho = int(input())
+
+    if(caminho == 2):
+        iteracoes += 1
+        sala +=2    
+        print("Você está na sala: ", sala)
+
+
+    elif(caminho != 2):
+        print("Só exite um caminho!")  
+            
+
+while (iteracoes < 7 and sala < 9) and sala != 6:  
+    
+    print("Escolha seu caminho: ")
+    print("[1] - Caminho vermelho")
+    print("[2] - Caminho preto")
+    caminho = int(input())
+
+    if(caminho == 1):
+        iteracoes += 1
+        sala +=1    
+        print("Você está na sala: ", sala)
+
+
+    elif(caminho == 2 and sala == 8):
+        sala = random.randint(1,5)
+        iteracoes += 1
+        print("Você está na sala: ", sala)
+
+    else:
+        iteracoes += 1
+        sala +=2    
+        print("Você está na sala: ", sala) 
+
 
 if iteracoes > 6 :
     print("Ultrapassou o Número de Jogadas Permitidas")
