c3c5430a94c5f7adf12b262498b543532e4062fc
LeonardoSa <33468255+LeoonardoSa@users.noreply.github.com>
2021-09-09 20:49:26 -0300

    Alterações

--- Atividade.py
+++ Atividade.py
@@ -1,11 +1,13 @@
+import random
 
 print("Você está na sala: 1")
 
 sala = 1
+iteracoes = 0
 
 
+while iteracoes < 7 and sala < 6:
 
-while sala < 9:
     print("Escolha seu caminho: ")
     print("[1] - Caminho vermelho")
     print("[2] - Caminho preto")
@@ -13,12 +15,57 @@ while sala < 9:
 
     if(caminho == 1):
         sala +=1
+        iteracoes += 1
         print("Você está na sala: ", sala)
 
 
     elif(caminho == 2):
+        iteracoes += 1
+        sala +=2    
+        print("Você está na sala: ", sala)
+
+if sala == 6:
+
+    while iteracoes < 7 and sala < 8:
+        
+        print("Escolha seu caminho: ")
+        print("[2] - Caminho preto")
+        caminho = int(input())
+
+        if(caminho == 2):
+            iteracoes += 1
+            sala +=2    
+            print("Você está na sala: ", sala)
+
+
+        elif(caminho != 2):
+            print("Só exite um caminho!")
+
+while (iteracoes < 7 and sala < 9) and sala != 6:  
+    
+    print("Escolha seu caminho: ")
+    print("[1] - Caminho vermelho")
+    print("[2] - Caminho preto")
+    caminho = int(input())
+
+    if(caminho == 1):
+        iteracoes += 1
+        sala +=1    
+        print("Você está na sala: ", sala)
+
+
+    elif(caminho == 2 and sala == 8):
+        sala = random.randint(1,5)
+        iteracoes += 1
+        print("Você está na sala: ", sala)
+
+    else:
+        iteracoes += 1
         sala +=2    
         print("Você está na sala: ", sala)
 
 
-print("PARABENS NERDOLA")
+if iteracoes > 6 :
+    print("Ultrapassou o Número de Jogadas Permitidas")
+elif sala == 9:
+    print("Parabéns Vocês Venceram Essa Dungeon")
